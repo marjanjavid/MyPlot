9313d5c42e517ef8e7ac5c32b984f85ecc91b619
DESKTOP-7OHVFTD\marjan <marjan.javid@gmail.com>
2018-02-19 14:19:38 +0330

    .

--- TestZebbleChart/App.UI/Views/Pages/Page1.zbl.cs
+++ TestZebbleChart/App.UI/Views/Pages/Page1.zbl.cs
@@ -15,35 +15,28 @@ namespace UI.Pages
     {
         public override async Task OnInitializing()
         {
-            var myPlotModel = new PlotModel
+            var myPlotModel = new Chart.PlotModel
             {
                 Title = "myPlotModeljon"
             };
-            myPlotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
-            myPlotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Maximum = 10, Minimum = 0 });
-            var series1 = new LineSeries
+            myPlotModel.Chart = new Chart.Line(new List<Chart.DataPoint>
             {
-                MarkerType = MarkerType.Circle,
-                MarkerSize = 4,
-                MarkerStroke = OxyColors.White
-            };
+                  new Chart.DataPoint(0.0, 6.0),
+                  new Chart.DataPoint(1.4, 2.1),
+                  new Chart.DataPoint(2.0, 4.2),
+                  new Chart.DataPoint(3.3, 2.3),
+                  new Chart.DataPoint(4.7, 7.4),
+                  new Chart.DataPoint(6.0, 6.2),
 
-            series1.Points.Add(new DataPoint(0.0, 6.0));
-            series1.Points.Add(new DataPoint(1.4, 2.1));
-            series1.Points.Add(new DataPoint(2.0, 4.2));
-            series1.Points.Add(new DataPoint(3.3, 2.3));
-            series1.Points.Add(new DataPoint(4.7, 7.4));
-            series1.Points.Add(new DataPoint(6.0, 6.2));
-            series1.Points.Add(new DataPoint(8.9, 8.9));
-
-            myPlotModel.Series.Add(series1);
+            });
             await base.OnInitializing();
 
             await myPlotView.Add(myPlotModel);
+
         }
 
-        
 
-        
+
+
     }
 }
