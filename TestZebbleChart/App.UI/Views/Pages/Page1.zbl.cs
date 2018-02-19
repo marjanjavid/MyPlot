0164fc93bc5430053d5a0c65d794af02d9d11414
DESKTOP-7OHVFTD\marjan <marjan.javid@gmail.com>
2018-02-19 17:24:39 +0330

    add linechart,pichart and area chart example

--- TestZebbleChart/App.UI/Views/Pages/Page1.zbl.cs
+++ TestZebbleChart/App.UI/Views/Pages/Page1.zbl.cs
@@ -15,11 +15,11 @@ namespace UI.Pages
     {
         public override async Task OnInitializing()
         {
-            var myPlotModel = new Chart.PlotModel
+            //line plotmodel
+            var linePlotModel = new Chart.PlotModel
             {
-                Title = "myPlotModeljon"
-            };
-            myPlotModel.Chart = new Chart.Line(new List<Chart.DataPoint>
+                Title = "zebbleLineChart",
+                Chart = new Chart.Line(new List<Chart.DataPoint>
             {
                   new Chart.DataPoint(0.0, 6.0),
                   new Chart.DataPoint(1.4, 2.1),
@@ -28,11 +28,40 @@ namespace UI.Pages
                   new Chart.DataPoint(4.7, 7.4),
                   new Chart.DataPoint(6.0, 6.2),
 
-            });
+            })
+            };
+            //area plotmodel
+            var areaPlotModel = new Chart.PlotModel
+            {
+                Title = "zebbleAreaChart",
+                Chart = new Chart.Area(new List<Chart.DataPoint>
+                {
+                    new Chart.DataPoint(0, 50),
+                    new Chart.DataPoint(10, 40),
+                    new Chart.DataPoint(20, 60),
+                    new Chart.DataPoint(0, 60),
+                    new Chart.DataPoint(5, 80),
+                    new Chart.DataPoint(20, 70)
+                })
+            };
+            //pie plotmodel
+            var piePlotModel = new Chart.PlotModel
+            {
+                Title = "zebbleAreaChart",
+                Chart = new Chart.Pie(new List<Chart.PieSlice>
+                {
+                    new Chart.PieSlice("Africa", 1030),
+                    new Chart.PieSlice("Americas", 929),
+                    new Chart.PieSlice("Asia", 4157),
+                    new Chart.PieSlice("Europe", 739),
+                    new Chart.PieSlice("Oceania", 35)
+                })
+            };
             await base.OnInitializing();
 
-            await myPlotView.Add(myPlotModel);
-
+            await linePlotView.Add(linePlotModel);
+            await areaPlotView.Add(areaPlotModel);
+            await piePlotView.Add(piePlotModel);
         }
 
 
