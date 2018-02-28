f67468dfaad87b7749bde9d502a679a55fa4f4aa
DESKTOP-7OHVFTD\marjan <marjan.javid@gmail.com>
2018-02-28 11:19:15 +0330

    Add CandleStick

---
+++ TestZebbleChart/App.UI/Views/Pages/ChartSamples/CandleStickChart.zbl.cs
@@ -0,0 +1,28 @@
+﻿namespace UI.Pages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Zebble;
+    using Domain;
+    using OxyPlot.Axes;
+
+    partial class CandleStickChart
+    {
+        public override async Task OnInitializing()
+        {
+            var startTimeValue = DateTimeAxis.ToDouble(new DateTime(2016, 1, 1));
+            var candleStickPlotModel = new Chart.PlotModel { Title = "zebbleCandleStick" };
+            candleStickPlotModel.Axes.Add(new DateTimeAxis { Position = AxisPosition.Bottom, Minimum = startTimeValue - 7, Maximum = startTimeValue + 7 });
+            candleStickPlotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left });
+            var series = new Chart.CandleStick();
+            series.Data.Add(new Chart.HighLowItem(startTimeValue, 100, 80, 92, 94));
+            series.Data.Add(new Chart.HighLowItem(startTimeValue + 1, 102, 77, 94, 93));
+            candleStickPlotModel.Series.Add(series);
+            await base.OnInitializing();
+            candleStickPlotView.Add(candleStickPlotModel);
+        }
+    }
+}
