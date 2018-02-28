9d9feec6050855fd4817d12b76d5a01396c72d7e
DESKTOP-7OHVFTD\marjan <marjan.javid@gmail.com>
2018-02-28 13:04:59 +0330

    Add FunctionChart

---
+++ TestZebbleChart/App.UI/Views/Pages/ChartSamples/FunctionChart.zbl.cs
@@ -0,0 +1,25 @@
+﻿namespace UI.Pages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Zebble;
+    using Domain;
+
+    partial class FunctionChart
+    {
+        public override async Task OnInitializing()
+        {
+            var functionPlotModel = new Chart.PlotModel { Title = "FunctionChart"  };
+
+            functionPlotModel.Series.Add(new Chart.Function(Math.Sin, -10, 10, 0.1, "sin(x)"));
+            functionPlotModel.Series.Add(new Chart.Function(Math.Cos, -10, 10, 0.1, "cos(x)"));
+            functionPlotModel.Series.Add(new Chart.Function(t => 5 * Math.Cos(t), t => 5 * Math.Sin(t), 0, 2 * Math.PI, 1000, "cos(t),sin(t)"));
+            await base.OnInitializing();
+            await functionPlotView.Add(functionPlotModel);
+
+        }
+    }
+}
