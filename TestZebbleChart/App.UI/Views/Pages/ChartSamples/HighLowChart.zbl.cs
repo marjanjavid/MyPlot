e03b4fc1cf94d928abe2d31c5f8c491c0e8ed90b
DESKTOP-7OHVFTD\marjan <marjan.javid@gmail.com>
2018-03-01 01:56:22 +0330

    Add HighLowChart

---
+++ TestZebbleChart/App.UI/Views/Pages/ChartSamples/HighLowChart.zbl.cs
@@ -0,0 +1,38 @@
+﻿namespace UI.Pages
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Zebble;
+    using Domain;
+    using OxyPlot.Axes;
+
+    partial class HighLowChart
+    {
+        public override async Task OnInitializing()
+
+        {
+            var highLowPlotModel = new Chart.PlotModel { Title = "zebbleHighLowChart" };
+            var s1 = new Chart.HighLow();
+            var r = new Random(314);
+            var price = 100.0;
+            for (int x = 0; x < 24; x++)
+            {
+                price = price + r.NextDouble() + 0.1;
+                var high = price + 10 + r.NextDouble() * 10;
+                var low = price - (10 + r.NextDouble() * 10);
+                var open = low + r.NextDouble() * (high - low);
+                var close = low + r.NextDouble() * (high - low);
+                s1.Data.Add(new Chart.HighLowItem(x, high, low, open, close));
+            }
+
+            highLowPlotModel.Series.Add(s1);
+            highLowPlotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, MaximumPadding = 0.3, MinimumPadding = 0.3 });
+            await base.OnInitializing();
+            await highLowPlotView.Add(highLowPlotModel);
+
+        }
+    }
+}
